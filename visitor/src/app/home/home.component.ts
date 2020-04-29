4feef43cbed130112efbf183d463427d9fd02df2
ellie <eleanor.young18@ncf.edu>
2020-04-29 16:55:32 -0400

    Updated single and group visits to fit per-visit tracking spec

--- visitor/src/app/home/home.component.ts
+++ visitor/src/app/home/home.component.ts
@@ -31,11 +31,22 @@ export class HomeComponent {
   // Connect Agency to Firebase collection
   agencyCollection: AngularFirestoreCollection<Visit>;
   visits: Observable<Visit[]>;
-  ID = 'A4'
+  AgencyID: string;
   code: number;
   time = firebase.firestore.FieldValue.serverTimestamp();
   groupNumber: number;
-  groupCode= 1000000;
+
+  // Group code is today's date YYYYMMDDHHmmss
+  // example: Apr 29, 2020 at 4:47:35 PM -> 20200429044735 (12 digits)
+  today = new Date();
+  dd = String(this.today.getDate()).padStart(2, '0');
+  mm = String(this.today.getMonth() + 1).padStart(2, '0'); // January is 0 so add 1
+  hh = String(this.today.getHours()).padStart(2, '0');
+  min = String(this.today.getMinutes()).padStart(2, '0');
+  sec = String(this.today.getSeconds()).padStart(2, '0');
+  yyyy = this.today.getFullYear();
+
+  groupCode = parseInt(this.yyyy.toString() + this.mm + this.dd + this.hh + this.min + this.sec);
 
   @Input() agency: Agency; // Here is a reference to the current agency that is logged in. Properties: name, id ( collection ) , room  
 
@@ -48,12 +59,12 @@ export class HomeComponent {
   }
 
   addSingleVisit() {
-    this.afs.collection(this.agency.id).add({ 'ID': this.ID, 'code': this.code, 'Time': this.time });
+    this.afs.collection(this.code.toString()).add({ 'AgencyID': this.agency.id, 'code': this.code, 'Time': this.time });
   }
 
   addGroupVisit(){
       for (var _i = 0; _i < this.groupNumber; _i++) {
-        this.afs.collection(this.agency.id).add({'ID':this.ID, 'code': this.groupCode, 'Time': this.time});
+        this.afs.collection(this.groupCode.toString()).add({ 'AgencyID': this.agency.id, 'code': this.groupCode, 'Time': this.time });
     }
   }
 
