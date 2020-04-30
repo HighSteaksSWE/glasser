32841010c1d2992e4a9e617c383850d03cc649e5
Jan Fic <43736433+janfic@users.noreply.github.com>
2020-04-30 18:26:27 -0400

    added stats component ( empty )

--- viewer/src/app/app.module.ts
+++ viewer/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
