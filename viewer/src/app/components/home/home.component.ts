40f27f04a203e454a67d9d395b557cf28a7d234f
Maria shehata <maria.sheahata17@ncf.edu>
2020-05-05 23:50:18 -0400

    small changes

--- viewer/src/app/components/home/home.component.ts
+++ viewer/src/app/components/home/home.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+// Firestore imports 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import {Observable,of, from, Timestamp } from 'rxjs';
+//import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+import * as firebase from 'firebase';
+import { NgForm, NgModel } from '@angular/forms';
+
+
+// interface that defines the agency visit structure
+interface  Visit{
+  ID: string;
+  code: number;
+  time: Timestamp<any>;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +23,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+   // Connect Agency to Firebase collection
+   agencyCollection: AngularFirestoreCollection<Visit>;
+   visits: Observable<Visit[]>;
+   AgencyID: string;
+   code: string;
+   time = firebase.firestore.FieldValue.serverTimestamp();
+   groupNumber: number;
+   size:number;
+ 
+
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    
   }
 
 }
