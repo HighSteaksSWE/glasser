92d9bc06830fd47630641a9ec21e5687cbb51613
Maria shehata <maria.sheahata17@ncf.edu>
2020-05-07 10:09:15 -0400

    Statistics about the frequency of multiple-agency visits

--- viewer/src/app/components/home/home.component.ts
+++ viewer/src/app/components/home/home.component.ts
@@ -9,12 +9,17 @@ import * as firebase from 'firebase';
 import { NgForm, NgModel } from '@angular/forms';
 
 
-// interface that defines the agency visit structure
+
 interface  Visit{
-  ID: string;
   code: number;
+}
+// interface that defines the agency visit structure
+interface  AencyVisit{
+  ID: string;
+  code: string;
   time: Timestamp<any>;
 }
+
 
 @Component({
   selector: 'app-home',
@@ -24,19 +29,50 @@ interface  Visit{
 export class HomeComponent implements OnInit {
 
    // Connect Agency to Firebase collection
-   agencyCollection: AngularFirestoreCollection<Visit>;
+   visitCollection: AngularFirestoreCollection<Visit>;
    visits: Observable<Visit[]>;
-   AgencyID: string;
-   code: string;
-   time = firebase.firestore.FieldValue.serverTimestamp();
-   groupNumber: number;
-   size:number;
- 
+   totalNumVisits = 0;
+   agencyCounter=0;
+   counterArray ={1:0, 2:0 , 3:0}; //  key represents duplicate or triplit of agencies, value = their number
+   mostCommonPair = {};
+   mostCommonTriplet = {};
 
   constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.getStatistics();
     
   }
 
+  getStatistics(): void{
+    const visitArray = this.afs.collection("visits").snapshotChanges();
+    visitArray.subscribe( payload => {
+      payload.forEach( item => {
+        const visitID = item.payload.doc.data() as Visit;
+        console.log("visitID", visitID.code);
+
+        
+        // getting visited Agenccies
+        const visitedAgencies = this.afs.collection(visitID.code.toString()).snapshotChanges();
+        visitedAgencies.subscribe( payload => {
+          payload.forEach( item => {
+            this.totalNumVisits+=1;
+            this.agencyCounter+=1;
+            const agencyVisit = item.payload.doc.data() as AencyVisit; 
+            console.log("agencyVisit", agencyVisit);
+           
+            
+          });
+          console.log(this.counterArray[this.agencyCounter]+=1,"visit for ", this.agencyCounter , "agencies" );
+          this.counterArray[this.agencyCounter]+=1;
+          this.agencyCounter =0;
+      });
+    });
+  });
+
+  console.log(this.counterArray[1] , " visit for " , 1 , "agencies");
+  console.log(this.counterArray[2] , " visit for " , 2 , "agencies");
+  console.log(this.counterArray[3] , " visit for " , 3 , "agencies");
+
+ }
 }
