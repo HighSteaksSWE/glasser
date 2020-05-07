68aec107b3117ddbd25a5ed208419ce82b4a3588
ellie <eleanor.young18@ncf.edu>
2020-05-06 23:03:46 -0400

    add login button

--- viewer/src/app/app.module.ts
+++ viewer/src/app/app.module.ts
@@ -6,26 +6,31 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StatsComponent } from './stats/stats.component';
 
+// Material Design
+import { MatButtonModule } from '@angular/material/button';
 
 // Firebase related imports
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import {ViewerServiceService } from './services/viewer-service.service';
+import { ViewerServiceService } from './services/viewer-service.service';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatsComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    MatButtonModule
   ],
   providers: [ViewerServiceService],
   bootstrap: [AppComponent]
---
+++ viewer/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  @Input() app: AppComponent;
+
+  successfulLogin() {
+    this.app.loginSuccessful = true;
+    console.log("logging tf in");
+  }
+
+  log(e) {
+    console.log(e);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
