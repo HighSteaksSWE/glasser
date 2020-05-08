5f5734fb6cc09addfd921c2cdccf0bf9d2fbda9a
Maria shehata <maria.sheahata17@ncf.edu>
2020-05-07 21:46:38 -0400

    getting data for frequency statistics

--- viewer/src/app/components/home/home.component.ts
+++ viewer/src/app/components/home/home.component.ts
@@ -36,6 +36,7 @@ export class HomeComponent implements OnInit {
    counterArray ={1:0, 2:0 , 3:0}; //  key represents duplicate or triplit of agencies, value = their number
    mostCommonPair = {};
    mostCommonTriplet = {};
+   agencyList = new Array(); 
 
   constructor(private afs: AngularFirestore) { }
 
@@ -52,27 +53,41 @@ export class HomeComponent implements OnInit {
         console.log("visitID", visitID.code);
 
         
-        // getting visited Agenccies
+        // getting visited frequencies
         const visitedAgencies = this.afs.collection(visitID.code.toString()).snapshotChanges();
         visitedAgencies.subscribe( payload => {
           payload.forEach( item => {
             this.totalNumVisits+=1;
             this.agencyCounter+=1;
             const agencyVisit = item.payload.doc.data() as AencyVisit; 
-            console.log("agencyVisit", agencyVisit);
-           
+            this.agencyList.push(agencyVisit.ID);
+            //console.log("agencyVisit", agencyVisit);          
             
           });
-          console.log(this.counterArray[this.agencyCounter]+=1,"visit for ", this.agencyCounter , "agencies" );
+          //console.log(this.counterArray[this.agencyCounter]+=1,"visit for ", this.agencyCounter , "agencies" );
           this.counterArray[this.agencyCounter]+=1;
+
+          // getting visited Agenccies based on the agencyCounter
+          for(var i = 0;i<this.agencyList.length;i++) { 
+            if (this.agencyCounter == 2) {
+              this.mostCommonPair[this.agencyList[i]] +=1;
+            } else if (this.agencyCounter == 3){
+              this.mostCommonTriplet[this.agencyList[i]] +=1; 
+            }
+          }
+
           this.agencyCounter =0;
+          //empty the array
+          this.agencyList.length = 0;
       });
     });
   });
-
+  
   console.log(this.counterArray[1] , " visit for " , 1 , "agencies");
   console.log(this.counterArray[2] , " visit for " , 2 , "agencies");
   console.log(this.counterArray[3] , " visit for " , 3 , "agencies");
+  console.log("this.mostCommonTriplet", this.mostCommonTriplet);
+  console.log("this.mostCommonPair", this.mostCommonPair);
 
  }
 }
