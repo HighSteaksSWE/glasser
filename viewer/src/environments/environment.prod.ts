ac2cd75079c42cdde3a93b9bdeeba9dca67e1112
Jan Fic <43736433+janfic@users.noreply.github.com>
2020-06-10 14:57:36 -0400

    deployment fix for viewer

--- viewer/src/environments/environment.prod.ts
+++ viewer/src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   firebase:{
     apiKey: "AIzaSyCrQGOFyGiPswII1umbB5dNRCvlyrXHmWQ",
-    authDomain: "glasser-viewer.firebaseapp.com",
-    databaseURL: "https://glasser-viewer.firebaseio.com",
-    projectId: "glasser-viewer",
-    storageBucket: "glasser-viewer.appspot.com",
+    authDomain: "glasser-ffb7a.firebaseapp.com",
+    databaseURL: "https://glasser-ffb7a.firebaseio.com",
+    projectId: "glasser-ffb7a",
+    storageBucket: "glasser-ffb7a.appspot.com",
     messagingSenderId: "573230320865",
     appId: "1:573230320865:web:76a86b0f4e9b1d8d84fba0"
   }
